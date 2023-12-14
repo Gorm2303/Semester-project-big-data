6e64b23cc5eacc4b79b61c680ece1a8b4c65030e
FrederikPT <frederik_tonnes@hotmail.com>
2023-12-14 14:08:32 +0100

    Added Some Error handling

--- Spark/python-analysis.py
+++ Spark/python-analysis.py
@@ -3,31 +3,53 @@ import zipfile
 from pyspark.sql import SparkSession
 
 def unzip_file(zip_filepath, extract_to):
-    with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
-        zip_ref.extractall(extract_to)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: analysis <file>", file=sys.stderr)
+    try:
+        with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
+            zip_ref.extractall(extract_to)
+    except FileNotFoundError:
+        print(f"File {zip_filepath} does not exist.", file=sys.stderr)
+        sys.exit(-1)
+    except zipfile.BadZipFile:
+        print(f"File {zip_filepath} is not a zip file.", file=sys.stderr)
+        sys.exit(-1)
+    except Exception as e:
+        print(f"An error occurred while unzipping {zip_filepath}: {str(e)}", file=sys.stderr)
         sys.exit(-1)
 
-    spark = SparkSession.builder.appName("PythonAnalysis").getOrCreate()
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: analysis <file1> <file2>", file=sys.stderr)
+        sys.exit(-1)
+
+    try:
+        spark = SparkSession.builder.appName("PythonAnalysis").getOrCreate()
+    except Exception as e:
+        print(f"An error occurred while creating a Spark session: {str(e)}", file=sys.stderr)
+        sys.exit(-1)
 
     # Unzip the files
     unzip_file(sys.argv[1], '/tmp/real_estate')
     unzip_file(sys.argv[2], '/tmp/yelp')
 
     # Read the unzipped files
-    real_estate_df = spark.read.option("header", "true").csv('/tmp/real_estate/*.csv')
-    yelp_df = spark.read.option("header", "true").json('/tmp/yelp/*.json')
+    try:
+        real_estate_df = spark.read.option("header", "true").csv('/tmp/real_estate/*.csv')
+        yelp_df = spark.read.option("header", "true").json('/tmp/yelp/*.json')
+    except Exception as e:
+        print(f"An error occurred while reading the data: {str(e)}", file=sys.stderr)
+        sys.exit(-1)
 
     # Save the DataFrames in different formats
-    real_estate_df.write.json("s3a://spark-data/real_estate.json")
-    real_estate_df.write.csv("s3a://spark-data/real_estate.csv")
-    real_estate_df.write.format("avro").save("s3a://spark-data/real_estate.avro")
+    try:
+        real_estate_df.write.json("s3a://spark-data/real_estate.json")
+        real_estate_df.write.csv("s3a://spark-data/real_estate.csv")
+        real_estate_df.write.format("avro").save("s3a://spark-data/real_estate.avro")
 
-    yelp_df.write.json("s3a://spark-data/yelp.json")
-    yelp_df.write.csv("s3a://spark-data/yelp.csv")
-    yelp_df.write.format("avro").save("s3a://spark-data/yelp.avro")
+        yelp_df.write.json("s3a://spark-data/yelp.json")
+        yelp_df.write.csv("s3a://spark-data/yelp.csv")
+        yelp_df.write.format("avro").save("s3a://spark-data/yelp.avro")
+    except Exception as e:
+        print(f"An error occurred while writing the data: {str(e)}", file=sys.stderr)
+        sys.exit(-1)
 
     spark.stop()
