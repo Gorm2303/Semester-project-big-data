65b1b67951f20a89c34bbb37ffffd664b1304ea9
FrederikPT <frederik_tonnes@hotmail.com>
2023-12-14 14:26:45 +0100

    Added Finally

--- Spark/python-analysis.py
+++ Spark/python-analysis.py
@@ -20,27 +20,20 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: analysis <file1> <file2>", file=sys.stderr)
         sys.exit(-1)
-
+    spark = None
     try:
         spark = SparkSession.builder.appName("PythonAnalysis").getOrCreate()
-    except Exception as e:
-        print(f"An error occurred while creating a Spark session: {str(e)}", file=sys.stderr)
-        sys.exit(-1)
 
-    # Unzip the files
-    unzip_file(sys.argv[1], '/tmp/real_estate')
-    unzip_file(sys.argv[2], '/tmp/yelp')
+        # Unzip the files
+        unzip_file(sys.argv[1], '/tmp/real_estate')
+        unzip_file(sys.argv[2], '/tmp/yelp')
 
-    # Read the unzipped files
-    try:
+        # Read the unzipped files
         real_estate_df = spark.read.option("header", "true").csv('/tmp/real_estate/*.csv')
         yelp_df = spark.read.option("header", "true").json('/tmp/yelp/*.json')
-    except Exception as e:
-        print(f"An error occurred while reading the data: {str(e)}", file=sys.stderr)
-        sys.exit(-1)
 
-    # Save the DataFrames in different formats
-    try:
+        # Save the DataFrames in different formats
+
         real_estate_df.write.json("s3a://spark-data/real_estate.json")
         real_estate_df.write.csv("s3a://spark-data/real_estate.csv")
         real_estate_df.write.format("avro").save("s3a://spark-data/real_estate.avro")
@@ -49,7 +42,10 @@ if __name__ == "__main__":
         yelp_df.write.csv("s3a://spark-data/yelp.csv")
         yelp_df.write.format("avro").save("s3a://spark-data/yelp.avro")
     except Exception as e:
-        print(f"An error occurred while writing the data: {str(e)}", file=sys.stderr)
+        print(f"An error occurred: {str(e)}", file=sys.stderr)
         sys.exit(-1)
+    finally:
+        if spark is not None:
+            spark.stop()
 
     spark.stop()
